4f2891cd4cbf170384dabc27ce51c4a764d7ab53
Taein142 <xodlsdldy@naver.com>
2024-02-20 14:17:28 +0900

    피드화면 출력 로직입니다.

--- semi-bookrep/src/main/java/com/semi/bookrep/service/FeedService.java
+++ semi-bookrep/src/main/java/com/semi/bookrep/service/FeedService.java
@@ -1,8 +1,17 @@
 package com.semi.bookrep.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.semi.bookrep.dao.ReportDao;
 
 @Service
 public class FeedService {
 
+	public List<ReportDao> getReportSummaryById(String userEmail) {
+		// DAO를 사용하여 데이터베이스에서 독후감 목록을 가져오는 로직
+		// 추가예정
+		return null;
+	}
 }
--- semi-bookrep/src/main/java/com/semi/bookrep/service/FollowService.java
+++ semi-bookrep/src/main/java/com/semi/bookrep/service/FollowService.java
@@ -4,5 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FollowService {
+	public int getFollowerValueById(String userEmail) {
+		// 팔로워 수 가져오는 메서드
+		// 추가 예정
+		return 0;
+	}
 
+	public int getFollowingValueById(String userEmail) {
+		// 팔로잉 수 가져오는 메서드
+		// 추가 예정
+		return 0;
+	}
 }
