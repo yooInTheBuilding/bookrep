6d1f8d75ce2ed59a2d6bebe1d0000e5f63263ef0
yooInTheBuilding <dbals9926@gmail.com>
2024-02-22 22:15:46 +0900

    before stash

--- semi-bookrep/src/main/java/com/semi/bookrep/controller/ReportCController.java
+++ semi-bookrep/src/main/java/com/semi/bookrep/controller/ReportCController.java
@@ -3,14 +3,20 @@ package com.semi.bookrep.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.semi.bookrep.dto.BookDTO;
 import com.semi.bookrep.dto.PageDTO;
+import com.semi.bookrep.dto.ReportDTO;
 import com.semi.bookrep.service.ReportCService;
 import com.semi.bookrep.util.MainUtil;
 
@@ -24,7 +30,7 @@ public class ReportCController {
 	private ReportCService reportCService;
 	
 	@GetMapping("write")
-	public String showReportWriter() {
+	public String showReportWriter(@ModelAttribute("book") BookDTO bookDTO) {
 		log.info("showReportWriter()");
 		return "write";
 	}
@@ -38,5 +44,21 @@ public class ReportCController {
 		return "bookSearch";
 	}
 	
+	@GetMapping("apply")
+	public String applyBookSearch(RedirectAttributes rttr, BookDTO bookDTO) {
+		log.info("applyBookSearch()");
+		reportCService.saveBook(bookDTO);
+		rttr.addFlashAttribute("book", bookDTO);
+		return "redirect:write";
+	}
+	
+	@PostMapping("save")
+	public String setReport(HttpSession session, ReportDTO reportDTO, RedirectAttributes rttr) {
+		log.info("setReport()");
+		
+		String view = reportCService.setReport(session, reportDTO, rttr);
+		
+		return view;
+	}
 	
 }
