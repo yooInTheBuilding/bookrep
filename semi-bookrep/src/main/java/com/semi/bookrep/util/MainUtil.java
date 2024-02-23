9e539569507b359557ab4763582d89be55ab478b
Taein142 <xodlsdldy@naver.com>
2024-02-23 17:45:26 +0900

    MainUtil -setPaging setPagingTest -setPagingTest PageDTO

--- semi-bookrep/src/main/java/com/semi/bookrep/util/MainUtil.java
+++ semi-bookrep/src/main/java/com/semi/bookrep/util/MainUtil.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 import com.semi.bookrep.dto.PageDTO;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MainUtil {
 	private static int maxNum=0;
 	
 	// 매개변수인 maxCnt는  메인 디스플레이에 표시되는 콘텐츠 개수
 	@SuppressWarnings("rawtypes")
 	public static <T> List<PageDTO> setPaging(List<T> objectList, int maxCnt){
+		log.info("setPaging()에 들어옴");
 		if (objectList.size() % maxCnt == 0) {
 			maxNum = objectList.size() / maxCnt;
 		}else {
@@ -21,7 +26,7 @@ public class MainUtil {
 		
 		for(int i=0; i<maxNum; i++) {
 			List<T> onePageList = new ArrayList<T>();
-			for (int j = maxCnt * i; j < maxCnt * (i + 1); j++) {
+			for (int j = maxCnt * i; j < maxCnt * (i + 1) && j < objectList.size(); j++) {
 				onePageList.add(objectList.get(j));
 			}
 			PageDTO<T> pageDTO = new PageDTO<T>(i + 1, onePageList);
