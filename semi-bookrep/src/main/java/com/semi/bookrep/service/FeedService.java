e51d703c650e625ee1470d7753d2f075d02c616e
Taein142 <xodlsdldy@naver.com>
2024-02-26 11:36:54 +0900

    Service -FeedServie   -getReportSummaryById

--- semi-bookrep/src/main/java/com/semi/bookrep/service/FeedService.java
+++ semi-bookrep/src/main/java/com/semi/bookrep/service/FeedService.java
@@ -47,25 +47,25 @@ public class FeedService {
 		List<Object> summaryList = new ArrayList<>();
 		log.info("이미지 매칭 작업 시작");
 		List<String> imagelist = new ArrayList<>();
-		List<Integer> likeList = new ArrayList<>();
 		
 		try {
-			for(ReportDTO reportDTO : userReports) {
-				imagelist.addAll(0, bookDao.getImageList(reportDTO.getBookIsbn()));
-				likeList.add(reportRService.getLikeValueByReportId(reportDTO.getId()));
-				
-				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("report", reportDTO);
-				map.put("image", imagelist.get(0));
-				map.put("like", likeList.get(0));
-				
-				summaryList.add(map);
-				log.info("잘 가져옴");
-			}
-		} catch (Exception e) {
-			log.info("이미지 못 받아옴.");
-			e.printStackTrace();
-		}
+	         for(ReportDTO reportDTO : userReports) {
+	            imagelist.addAll(0, bookDao.getImageList(reportDTO.getBookIsbn()));
+	            int likeValue = reportRService.getLikeValueByReportId(reportDTO.getId());
+	            
+	            Map<String, Object> map = new HashMap<String, Object>();
+	            map.put("report", reportDTO);
+	            map.put("image", imagelist.get(0));
+	            map.put("like", likeValue);
+	            
+	            summaryList.add(map);
+	            log.info("잘 가져옴");
+	         }
+	      } catch (Exception e) {
+	         log.info("이미지 못 받아옴.");
+	         e.printStackTrace();
+	      }
+
 	
 		log.info("setPaging() 진입시도");
 		
