d70fba40b408e09763c23c81286c1554d3cd7a0d
Taein142 <xodlsdldy@naver.com>
2024-03-03 14:50:17 +0900

    update like-logic and bookDetail-front

--- semi-bookrep/src/main/java/com/semi/bookrep/service/ReportRService.java
+++ semi-bookrep/src/main/java/com/semi/bookrep/service/ReportRService.java
@@ -17,56 +17,64 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class ReportRService {
-	
-	@Autowired
-	private ReportDao reportDao;
-	
-	@Autowired
-	private LikeDao likeDao;
-	
-	@Autowired
-	private CommentDao commentDao;
-	
+   
+   @Autowired
+   private ReportDao reportDao;
+   
+   @Autowired
+   private LikeDao likeDao;
+   
+   @Autowired
+   private CommentDao commentDao;
+   
 
-	public ReportDTO getReportDetailByReportId(Long id) {
-		log.info("getReportDetailByReportId()");
-		
-		ReportDTO reportDTO = reportDao.getReportDetailByReportId(id);
-		
-		return reportDTO;
-	}
+   public ReportDTO getReportDetailByReportId(Long id) {
+      log.info("getReportDetailByReportId()");
+      
+      ReportDTO reportDTO = reportDao.getReportDetailByReportId(id);
+      
+      return reportDTO;
+   }
 
-	public List<CommentDTO> getCommentByReportId(Long id) {
-		log.info("getCommentByReportId()");
-		
-		List<CommentDTO> commentList = commentDao.getCommentByReportId(id);
-		
-		return commentList;
-	}
+   public List<CommentDTO> getCommentByReportId(Long id) {
+      log.info("getCommentByReportId()");
+      
+      List<CommentDTO> commentList = commentDao.getCommentByReportId(id);
+      
+      return commentList;
+   }
 
-	public Integer getLikeValueByReportId(Long id) {
-		log.info("getLikeValueByReportId()");
-		
-		Integer likeValue = likeDao.getLikeValueByReportId(id);
-		
-		return likeValue;
-	}
+   public Integer getLikeValueByReportId(Long id) {
+      log.info("getLikeValueByReportId()");
+      
+      Integer likeValue = likeDao.getLikeValueByReportId(id);
+      
+      return likeValue;
+   }
 
-	public void setLike(String email, Long id) {
-		log.info("setLike()");
-		
-		LikeDTO likeDTO = new LikeDTO(email, id);
-		likeDao.setLike(likeDTO);
-	}
+   public void setLike(String email, Long id) {
+      log.info("setLike()");
+      
+      LikeDTO likeDTO = new LikeDTO(email, id);
+      
+      boolean isLiked = this.getLikeValueByReportId(id) > 0;
+      
+       // 좋아요 토글
+       if (isLiked) {
+           likeDao.removeLike(likeDTO);
+       } else {
+           likeDao.setLike(likeDTO);
+       }
+   }
 
-	public void setComment(String email, Long id, String content) {
-		log.info("setComment()");
-		
-		CommentDTO commentDTO = new CommentDTO();
-		commentDTO.setReportId(id);
-		commentDTO.setContent(content);
-		commentDTO.setUserEmail(email);
-		commentDao.setComment(commentDTO);
-	}
+   public void setComment(String email, Long id, String content) {
+      log.info("setComment()");
+      
+      CommentDTO commentDTO = new CommentDTO();
+      commentDTO.setReportId(id);
+      commentDTO.setContent(content);
+      commentDTO.setUserEmail(email);
+      commentDao.setComment(commentDTO);
+   }
 
 }
